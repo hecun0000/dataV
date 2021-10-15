09298f05930ba1752796bc7508be1682a2b92434
hecun <1639586328@qq.com>
2021-10-15 09:37:36 +0800

    first commit

---
+++ .history/vite.config_20211013162055.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import ElementPlus from 'unplugin-element-plus/vite'
+
+// 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D
+import { resolve } from 'path'
+import legacy from '@vitejs/plugin-legacy'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), ElementPlus({}),
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+    }
+  },
+  base: './', // 设置打包路径
+  server: {
+    port: 4500, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+    cors: true // 允许跨域
+
+    // 设置代理，根据我们项目实际情况配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:x000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/')
+    //   }
+    // },
+  }
+})
---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="main-container">
+    <div class="top">
+      <Header />
+    </div>
+
+    <div class="bottom">
+      <div class="left">
+        <Nav />
+      </div>
+
+      <div class="right">
+        <div class="content">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from '@/components/Header.vue'
+import Nav from '@/components/Nav.vue'
+
+export default defineComponent({
+  name: 'Main',
+  components: {
+    Header,
+    Nav
+  }
+})
+</script>
+
+<style scoped>
+</style>
